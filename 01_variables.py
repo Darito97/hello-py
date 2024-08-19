2366f47bc64e509262f8abcf0d56ed37f6ffe55d
David Alonso <davidalonso991@gmail.com>
2024-08-18 20:09:31 -0600

    Add(variables): Crear algunas variables con convecciones de python

---
+++ 01_variables.py
@@ -0,0 +1,14 @@
+## los nombres de las variables deben ser con texto, con guion bajo y no deben comenzar en numero
+## Por conveccion debe ir en minusculas y con guion bajo
+my_int_variable = 1
+print(my_int_variable)
+
+my_string_variable = "Hola"
+print(my_string_variable)
+
+my_boolean_variable = True
+print(my_boolean_variable)
+
+my_Array_variable = [1,3,4,6]
+print(my_Array_variable)
+
