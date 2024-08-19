ecc8cc1a479a7a8121c9f2032140d406af182eef
David Alonso <davidalonso991@gmail.com>
2024-08-18 18:50:44 -0600

    Add(Strings): Comilla doble y comilla simple para strings

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 # Este es un comentario
 print("Hola mundo");
+# Las cadenas de texto pueden ir entre comillas simples o dobles
+print('Hola mundo');
 
 """Este es un comentario multilinea
 Aqui te puedes expandir todo lo que quieras"""
 
+ 
+ 
