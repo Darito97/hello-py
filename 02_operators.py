f035cb053cc8c3dcb9c16070191b6a07d71f6178
David Alonso <davidalonso991@gmail.com>
2024-08-21 11:28:21 -0600

    Add(operators): Agregado de operadores comparativos

--- 02_operators.py
+++ 02_operators.py
@@ -1,5 +1,5 @@
 
-### Operaciones
+### Operaciones con operadores aritmeticos
 print(3 + 4)
 print(3 - 4)
 print(3 * 4)
@@ -13,5 +13,21 @@ print(3 ^ 4) ### XOR
 print("Hola " + "Python"); ### Concatenación de cadenas
 print("hola " + str(5)) ### Concatenación de cadena con un número para evitar errores
 print("Hola " * 5) ### Repetición de cadenas
+
+
+### Operadores comparativos
+
+
+print(3 == 4) ### Igualdad
+print(3 != 4) ### Diferencia
+print(3 > 4) ### Mayor que
+print(3 < 4) ### Menor que 
+print(3 >= 4) ### Mayor o igual que
+print(3 <= 4) ### Menor o igual que
+print(3 is 4) ### Identidad
+print(3 is not 4) ### Negación de identidad
+print(3 in [1,2,3,4,5]) ### Pertenencia
+print(3 not in [1,2,3,4]) ### Negación de pertenencia
+
 
 
