035c2acf83a0d6b74f826b6a0629c9b3989de0fc
David Alonso <davidalonso991@gmail.com>
2024-08-21 11:15:03 -0600

    Add(operators): Uso de algunos operadores

---
+++ 02_operators.py
@@ -0,0 +1,10 @@
+
+### Operaciones
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4) ### Modulo o residuo
+print(3 ** 4) ### Potencia
+print(3 // 4) ### División entera
+print(3 ^ 4) ### XOR
