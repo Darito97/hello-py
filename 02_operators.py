ac28fdcff68d11402e925925b7a9469adc82672d
David Alonso <davidalonso991@gmail.com>
2024-08-21 11:21:58 -0600

    Add(operators): Uso de algunas operaciones con cadenas

--- 02_operators.py
+++ 02_operators.py
@@ -8,5 +8,10 @@ print(3 % 4) ### Modulo o residuo
 print(3 ** 4) ### Potencia
 print(3 // 4) ### División entera
 print(3 ^ 4) ### XOR
+
+
 print("Hola " + "Python"); ### Concatenación de cadenas
+print("hola " + str(5)) ### Concatenación de cadena con un número para evitar errores
+print("Hola " * 5) ### Repetición de cadenas
+
 
