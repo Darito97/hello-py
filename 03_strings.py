af7af57d114b72ec0f3459458581ae32907d1bfd
David Alonso <davidalonso991@gmail.com>
2024-08-24 16:43:04 -0600

    Add(strings): Uso de algunas operaciones con strings

---
+++ 03_strings.py
@@ -0,0 +1,15 @@
+### Strings ###
+
+my_variable_string = "Hola Python"
+my_other_variable_string = "Hola Python"
+
+print(my_variable_string == my_other_variable_string) ### True
+
+print(my_variable_string + " " + my_other_variable_string) ### Contatenación de cadenas
+
+my_new_line_string = "Hola\nPython"
+print(my_new_line_string) ### String con salto de linea
+
+my_string_with_tab = "Hola\tPython"
+print(my_string_with_tab) ### String con tabulación
+
