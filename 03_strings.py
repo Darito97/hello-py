af5e19c39bd764cba4ec04fe237809a0f830dfd4
David Alonso <davidalonso991@gmail.com>
2024-08-24 16:52:14 -0600

    Add(strings): Formateo y buenas practias de cadenas

--- 03_strings.py
+++ 03_strings.py
@@ -3,13 +3,20 @@
 my_variable_string = "Hola Python"
 my_other_variable_string = "Hola Python"
 
-print(my_variable_string == my_other_variable_string) ### True
+print(my_variable_string == my_other_variable_string) # True
 
-print(my_variable_string + " " + my_other_variable_string) ### Contatenación de cadenas
+print(my_variable_string + " " + my_other_variable_string) # Contatenación de cadenas
 
 my_new_line_string = "Hola\nPython"
-print(my_new_line_string) ### String con salto de linea
+print(my_new_line_string) # String con salto de linea
 
 my_string_with_tab = "Hola\tPython"
-print(my_string_with_tab) ### String con tabulación
+print(my_string_with_tab) # String con tabulación
 
+
+### Formateo de strings ###
+
+name, surname, age = "David", "Alonso", 27
+
+print("Mi nombre es %s %s y tengo %s años" %(name, surname, age))
+print("Mi nombre es {} {} y tengo {} años".format(name, surname, age))
