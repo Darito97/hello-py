2ad6f83672e3e2c96fa286edd71af2f2c8261287
David Alonso <davidalonso991@gmail.com>
2024-08-24 21:24:16 -0600

    Add(tuples): Uso de tuplas y sus diferencias con listas

---
+++ 05_tuples.py
@@ -0,0 +1,30 @@
+### Tuplas ###
+
+
+### Como crear una tupla ###
+
+my_tuple = tuple()
+print(my_tuple)
+
+other_tuple = ()
+print(other_tuple)
+
+my_tuple = (1, "David", 27, 1.78)
+print(my_tuple)
+print(type(my_tuple))
+
+# Acceder a un elemento de una tupla
+print(my_tuple[2])
+
+# Saber cuantos elementos de un dato hay en una tupla
+print(my_tuple.count("David"))
+
+# Obtener índice de un elemento
+print(my_tuple.index(27))
+
+# Las diferencias entre una lista y una tupla son:
+# Las tuplas son inmutables, es decir, no se pueden modificar
+# Las tuplas son más rápidas que las listas
+# Las tuplas son más seguras que las listas
+# Las tuplas son usadas para proteger los datos de la modificación
+
