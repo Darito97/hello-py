17fecfeb3728f0f85eef63f762cb624bfeeb3082
David Alonso <davidalonso991@gmail.com>
2024-08-24 18:26:35 -0600

    Add(lists): Obtenido de valores con indices

--- 04_lists.py
+++ 04_lists.py
@@ -11,3 +11,23 @@ print(my_list)
 # Otra forma de definir una lista
 my_list = []
 print(my_list)
+
+#len nos permite obtener la longitud de una lista
+print(len(my_list))
+my_list = [1,2,3,4]
+print(len(my_list))
+
+
+# Tipos de datos en una lista
+my_list = [27, "David", 1.78]
+print(my_list)
+
+# Tipo de dato de una lista
+print(type(my_list))
+
+# Acceder a un elemento de la lista
+print(my_list[2])
+
+# Si accedemos a un índice negativo, se accede a la lista de derecha a izquierda
+print(my_list[-2])
+
