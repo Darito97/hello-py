766855a21687f5b41874b187ddd737ba35d14c3c
David Alonso <davidalonso991@gmail.com>
2024-08-24 18:14:44 -0600

    Add(lists): Otras formas de definición de listas

--- 04_lists.py
+++ 04_lists.py
@@ -4,3 +4,10 @@
 my_list = [1,2,3,4]
 print(my_list)
 
+# Otro ejemplo de definición de una lista
+my_list = list()
+print(my_list)
+
+# Otra forma de definir una lista
+my_list = []
+print(my_list)
