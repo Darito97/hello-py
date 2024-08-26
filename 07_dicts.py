b4dbfd6131a4cd3848415698a405a2bb2e1f6266
David Alonso <davidalonso991@gmail.com>
2024-08-26 00:58:23 -0600

    Add(dicts): Obtenido de longitud y acceso a un valor

--- 07_dicts.py
+++ 07_dicts.py
@@ -6,8 +6,18 @@ my_dict = {
     "name": "David",
     "age": 27,
     "height": 1.78,
-    "is_developer": True
+    "is_developer": True,
+    1: 1
 }
 my_other_dict = dict(name="David", age=27, height=1.78, is_developer=True)
 print(my_dict, type(my_dict))
 print(my_other_dict, type(my_other_dict))
+
+
+# Obtener la longitud de pares clave-valor en un diccionario
+print(len(my_dict))
+
+
+# Acceder a uno de los valores por clave
+
+print(my_dict["name"])
