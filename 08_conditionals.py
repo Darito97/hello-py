2f7ce010f4ceebacf124707b16decc0b15d15132
David Alonso <davidalonso991@gmail.com>
2024-08-26 21:20:46 -0600

    Add(conditionals): Agregado de condicional de igualación y diferente

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -21,3 +21,25 @@ else:
     print("La condición es falsa")
     
 print("Fin de la condición if-else")
+
+
+# Condicional con igualación
+my_number = 15
+
+if my_number == 15:
+    print("El número es 15")
+else:
+    print("El número no es 15")
+
+print("Fin de la condición de igualación")
+
+# Condicional con diferente
+
+my_number = -15
+
+if my_number != 15:
+    print("El número no es 15")
+else:
+    print("El número es 15")
+
+print("Fin de la condición de diferente")
